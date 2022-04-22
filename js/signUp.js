c376b686e7dd1ed9abb83dd5239e290c1b02b865
chakravarthi-v <chakravarthiviswanath@gmail.com>
2022-04-22 10:29:03 +0530

    signup-integration with firebase

--- js/signUp.js
+++ js/signUp.js
@@ -17,33 +17,6 @@
 
 const username=document.getElementById("username-field");
 const password=document.getElementById("password-field");
-const login=document.getElementById("signin-btn");
-login.addEventListener('click',e=>{
-    e.preventDefault();
-    const usernameValue=username.value;
-    const passwordValue=password.value;
-    const auth=firebase.auth();
-    
-    console.log(usernameValue);
-    console.log(passwordValue);
-    if(usernameValue==""){
-        alert("Please enter your username");
-    }
-    else if(passwordValue==""){
-        alert("Please enter your password");
-    }
-    else{
-      auth.signInWithEmailAndPassword(usernameValue, passwordValue).then(user =>{
-        alert("Login Successful :)");
-    }).catch(err => {
-        alert(err.message);
-    });
-      
-    }
-
-});
-const userName=document.getElementById("user-name");
-const passWord=document.getElementById("pass-word");
 const signUp=document.getElementById("signup-btn");
 signUp.addEventListener('click',e=>{
   e.preventDefault();
